3a27b3f084ec2b347ab79fc459c74f7d78bf9d29
bricezakra <bricezakra@live.com>
2019-10-10 10:50:25 -0500

    server.js updated

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+var PORT = process.env.PORT || 3000;
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+var reserved = [];
+var waiting = [];
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+app.listen(PORT, function() {
+    console.log("tuned in to port 3000")
+})
